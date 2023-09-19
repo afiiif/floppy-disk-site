312fe3b154b88866f78bdcc3a35ae180856f3311
M. Afifudin <afif.x.udin@gmail.com>
2023-09-19 17:19:35 +0700

    feat: update homepage & og-image

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { useRouter } from 'next/router';
+import { useConfig } from 'nextra-theme-docs';
 
 /** @type {import('nextra-theme-docs').DocsThemeConfig} */
 const themeConfig = {
@@ -16,15 +17,21 @@ const themeConfig = {
   useNextSeoProps: () => ({
     titleTemplate: '%s – Floppy Disk',
   }),
-  head: (
-    <>
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-      <link rel="icon" type="image/x-icon" href="/favicon/favicon.ico" />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
-    </>
-  ),
+  head: () => {
+    const { frontMatter } = useConfig();
+    const ogImage = frontMatter.image || 'https://floppy-disk.vercel.app/floppy-disk-banner.jpg';
+    return (
+      <>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="icon" type="image/x-icon" href="/favicon/favicon.ico" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <meta property="og:image" content={ogImage} />
+        <meta name="twitter:image" content={ogImage} />
+      </>
+    );
+  },
   gitTimestamp: ({ timestamp }) => {
     const { route, locale } = useRouter();
     if (route === '/') return null;
