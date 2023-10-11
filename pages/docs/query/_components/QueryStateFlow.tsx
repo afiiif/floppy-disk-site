ddfa34f1fa51405bfdea9d3e226aff928258c53e
M. Afifudin <afif.x.udin@gmail.com>
2023-10-12 03:42:42 +0700

    feat: update query state flow

--- pages/docs/query/_components/QueryStateFlow.tsx
+++ pages/docs/query/_components/QueryStateFlow.tsx
@@ -4,20 +4,32 @@ export default function QueryStateFlow() {
     <section className="-mr-6 overflow-x-auto pb-4 pt-8">
       <div className="relative w-[41rem] text-sm">
         <svg
-          viewBox="0 0 440 500"
+          viewBox="0 0 440 550"
           stroke="rgba(120 120 120)"
           strokeWidth={2}
           fill="none"
           strokeLinecap="round"
           strokeLinejoin="round"
-          className="pointer-events-none absolute top-[10.6rem]"
+          className="pointer-events-none absolute top-[10.6rem] -z-10"
           width={486}
         >
           <path d="M15.593 3.607v34.088" />
           <path d="M23.751 171.619a8.16 8.16 0 0 1-8.158-8.158v-49.344m30.009 57.502H23.751M7.79 29.892l7.803 7.803 7.803-7.803M37.8 179.423l7.803-7.803-7.803-7.803" />
-          <path d="M23.751 398.927a8.16 8.16 0 0 1-8.158-8.158V163.816m30.009 235.111H23.751" />
-          <path d="M37.799 406.731l7.803-7.803-7.803-7.803m377.243-211.702l7.803-7.803-7.803-7.803m7.802 7.802h-34.079m-162.389 7.804l7.803-7.803-7.803-7.803m7.802 7.802h-34.079m26.275 235.112l7.803-7.803-7.803-7.803m7.803 7.802h-34.079M269.95 247.01a8.16 8.16 0 0 0 8.158-8.158v-14.344m-77.009 22.502h68.851m-61.048 7.804l-7.803-7.803 7.803-7.803M269.95 475.01a8.16 8.16 0 0 0 8.158-8.158v-14.344m-77.009 22.502h68.851" />
-          <path d="M208.902 482.814l-7.803-7.803 7.803-7.803m61.048-183.896a8.16 8.16 0 0 1 8.158 8.158v85.344m-77.009-93.502h68.851m-61.047-7.804l-7.803 7.803 7.803 7.803" />
+          <path d="M23.751 366.927a8.16 8.16 0 0 1-8.158-8.158V163.816m30.009 203.111H23.751" />
+          <path d="M37.799 374.731l7.802-7.803-7.802-7.804" transform-origin="41.7px 376.928px" />
+          <path d="M415.042 179.423l7.803-7.803-7.803-7.803m7.802 7.802h-34.079m-162.389 7.804l7.803-7.803-7.803-7.803m7.802 7.802h-34.079" />
+          <path
+            d="M226.375 374.731l7.802-7.803-7.802-7.803"
+            transform-origin="230.276px 376.928px"
+          />
+          <path d="M234.177 366.927h-34.079M269.95 247.01a8.16 8.16 0 0 0 8.158-8.158v-14.344m-77.009 22.502h68.851m-61.048 7.804l-7.803-7.803 7.803-7.803M269.95 526.01a8.16 8.16 0 0 0 8.158-8.158v-99.344M201.099 526.01h68.851" />
+          <path
+            d="M208.902 533.814l-7.802-7.803 7.802-7.804"
+            transform-origin="205.001px 516.01px"
+          />
+          <path d="M269.95 283.312a8.16 8.16 0 0 1 8.158 8.158v52.344m-77.009-60.502h68.851m-61.047-7.804l-7.803 7.803 7.803 7.803" />
+          <path d="M78.72 469.473v32.088" />
+          <path d="M70.917 493.758l7.803 7.803 7.803-7.803" />
         </svg>
 
         <div className="card">
@@ -63,8 +75,11 @@ export default function QueryStateFlow() {
               </li>
             </ul>
           </div>
-          <div className="card -mb-14">
-            <b>After refetch failed</b>
+          <div className="card">
+            <b>
+              After refetch failed
+              <div className="block text-xs font-normal">And no more retry</div>
+            </b>
             <ul>
               <li>status: "success"</li>
               <li>isLoading: false</li>
@@ -76,25 +91,16 @@ export default function QueryStateFlow() {
               <li className="mt-2 border-t border-dashed pt-1.5">
                 isRefetchError: <span className="text-green-500">true</span>
               </li>
-              <li className="mt-2 border-t border-dashed pt-1.5">
-                isGoingToRetry: <span className="text-blue-600">bool</span>
-                <div className="pt-1 text-xs italic">(depends on option 👆)</div>
-              </li>
+              <li>isGoingToRetry: false</li>
             </ul>
           </div>
         </div>
 
-        <div className="flex items-start gap-12 pl-14 pt-12">
-          <div className="card">
+        <div className="-mt-6 flex items-start gap-12 pl-14">
+          <div className="card waiting-state">
             <b>After failed</b>
             <ul>
-              <li>status: "error"</li>
-              <li>isLoading: false</li>
-              <li>isSuccess: false</li>
-              <li>
-                isError: <span className="text-green-500">true</span>
-              </li>
-              <li className="mt-2 border-t border-dashed pt-1.5">isWaiting: false</li>
+              <li>isWaiting: false</li>
               <li className="mt-2 border-t border-dashed pt-1.5">
                 isGoingToRetry: <span className="text-blue-600">bool</span>
                 <div className="pt-1 text-xs italic">(depends on option 👆)</div>
@@ -109,6 +115,23 @@ export default function QueryStateFlow() {
               </li>
             </ul>
           </div>
+        </div>
+
+        <div className="card ml-14 mt-12">
+          <b>
+            After failed
+            <div className="block text-xs font-normal">And no more retry</div>
+          </b>
+          <ul>
+            <li>status: "error"</li>
+            <li>isLoading: false</li>
+            <li>isSuccess: false</li>
+            <li>
+              isError: <span className="text-green-500">true</span>
+            </li>
+            <li className="mt-2 border-t border-dashed pt-1.5">isWaiting: false</li>
+            <li className="mt-2 border-t border-dashed pt-1.5">isGoingToRetry: false</li>
+          </ul>
         </div>
       </div>
 
