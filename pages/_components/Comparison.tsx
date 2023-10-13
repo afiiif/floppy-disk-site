8a566e194ce3020f6208357fe840767ff3f3dacf
M. Afifudin <afif.x.udin@gmail.com>
2023-10-13 21:11:56 +0700

    feat: update package version

--- pages/_components/Comparison.tsx
+++ pages/_components/Comparison.tsx
@@ -6,38 +6,51 @@ export function BadgeFD() {
   return (
     <>
       <a href="https://pkg-size.dev/floppy-disk" target="_blank" rel="noreferrer">
-        <img src="https://pkg-size.dev/badge/bundle/10742" alt="Bundle size for floppy-disk" />
+        <img src="https://pkg-size.dev/badge/bundle/12066" alt="Bundle size for floppy-disk" />
         <span className="nx-sr-only nx-select-none"> (opens in a new tab)</span>
       </a>
-      <div className="whitespace-nowrap pt-1 text-xs">(gzipped 3.8 kB)</div>
+      <div className="whitespace-nowrap pt-1 text-xs">(4.3 kB gzip)</div>
     </>
   );
 }
 export function BadgeZustand() {
   return (
-    <a href="https://pkg-size.dev/zustand?no-peers" target="_blank" rel="noreferrer">
-      <img src="https://pkg-size.dev/badge/bundle/4875" alt="Bundle size for zustand" />
-      <span className="nx-sr-only nx-select-none"> (opens in a new tab)</span>
-    </a>
+    <>
+      <a href="https://pkg-size.dev/zustand?no-peers" target="_blank" rel="noreferrer">
+        <img src="https://pkg-size.dev/badge/bundle/4877" alt="Bundle size for zustand" />
+        <span className="nx-sr-only nx-select-none"> (opens in a new tab)</span>
+      </a>
+      <div className="whitespace-nowrap pt-1 text-xs">(2 kB gzip)</div>
+    </>
   );
 }
 export function BadgeRQ() {
   return (
-    <a href="https://pkg-size.dev/@tanstack/react-query?no-peers" target="_blank" rel="noreferrer">
-      <img
-        src="https://pkg-size.dev/badge/bundle/47262"
-        alt="Bundle size for @tanstack/react-query"
-      />
-      <span className="nx-sr-only nx-select-none"> (opens in a new tab)</span>
-    </a>
+    <>
+      <a
+        href="https://pkg-size.dev/@tanstack/react-query?no-peers"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src="https://pkg-size.dev/badge/bundle/47285"
+          alt="Bundle size for @tanstack/react-query"
+        />
+        <span className="nx-sr-only nx-select-none"> (opens in a new tab)</span>
+      </a>
+      <div className="whitespace-nowrap pt-1 text-xs">(13 kB gzip)</div>
+    </>
   );
 }
 export function BadgeSWR() {
   return (
-    <a href="https://pkg-size.dev/swr?no-peers" target="_blank" rel="noreferrer">
-      <img src="https://pkg-size.dev/badge/bundle/11227" alt="Bundle size for swr" />
-      <span className="nx-sr-only nx-select-none"> (opens in a new tab)</span>
-    </a>
+    <>
+      <a href="https://pkg-size.dev/swr?no-peers" target="_blank" rel="noreferrer">
+        <img src="https://pkg-size.dev/badge/bundle/11227" alt="Bundle size for swr" />
+        <span className="nx-sr-only nx-select-none"> (opens in a new tab)</span>
+      </a>
+      <div className="whitespace-nowrap pt-1 text-xs">(5.3 kB gzip)</div>
+    </>
   );
 }
 
