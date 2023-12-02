7058f75fafc120dcbb811973f7df17b68089f5c7
M. Afifudin <afif.x.udin@gmail.com>
2023-12-02 20:00:26 +0700

    feat: update banner & title

--- pages/_components/Banner.tsx
+++ pages/_components/Banner.tsx
@@ -4,20 +4,10 @@ export default function Banner() {
       <div className="absolute left-0 z-10 -mt-4 w-full overflow-x-hidden border-b border-b-gray-200 dark:border-b-gray-800">
         <div className="bg-[radial-gradient(var(--tw-gradient-stops))] from-white dark:from-black">
           <div className="animate-trigger mx-auto max-w-6xl px-6 py-24 text-center md:px-12 xl:py-28">
-            <div className="relative">
-              <div
-                aria-hidden
-                className="pointer-events-none absolute w-full bg-gradient-to-b from-white/0 from-40% to-black/20 bg-clip-text pb-4 text-4xl font-extrabold text-transparent md:text-5xl xl:text-6xl"
-              >
-                4.3 kB Powerful
-                <div>[Global/Local, Sync/Async]</div>
-                State Management Library
-              </div>
-            </div>
-            <h1 className="text-4xl font-extrabold md:text-5xl xl:text-6xl">
-              4.3 kB Powerful
-              <div className="text-green-600 dark:text-green-400">[Global/Local, Sync/Async]</div>
-              State Management Library
+            <h1 className="-mb-3 bg-gradient-to-b from-zinc-700 to-zinc-900 bg-clip-text pb-3 text-4xl font-extrabold text-transparent selection:bg-black/70 selection:text-white dark:from-white dark:from-40% dark:to-gray-400 dark:selection:bg-white/70 dark:selection:text-black md:text-5xl xl:text-6xl">
+              Sync/Async
+              <div>Global State Manager</div>
+              Inside/Outside React
             </h1>
             <div className="animate-shake pt-6 text-xs opacity-40">
               Yes, another state management library alternative 👀
@@ -53,9 +43,9 @@ export default function Banner() {
       {/* For spacer only */}
       <div className="invisible -mt-4 mb-8 py-24 text-center md:mb-12 xl:-mx-16 xl:py-28">
         <div className="text-4xl font-extrabold md:text-5xl xl:text-6xl">
-          4.3 kB Powerful
-          <div>[Global/Local, Sync/Async]</div>
-          State Management Library
+          Sync/Async
+          <div>Global State Manager</div>
+          Inside/Outside React
         </div>
         <div className="pt-6 text-xs opacity-70">
           Yes, another state management library alternative 👀
