ba3afe31b2b9bdee023ecaeab320a85359f23c86
M. Afifudin <afif.x.udin@gmail.com>
2023-12-06 00:09:23 +0700

    style: update banner bg opacity

--- pages/_components/Banner.tsx
+++ pages/_components/Banner.tsx
@@ -2,7 +2,7 @@ export default function Banner() {
   return (
     <>
       <div className="absolute left-0 z-10 -mt-4 w-full overflow-x-hidden border-b border-b-gray-200 dark:border-b-gray-800">
-        <div className="bg-[radial-gradient(var(--tw-gradient-stops))] from-white dark:from-black">
+        <div className="bg-[radial-gradient(var(--tw-gradient-stops))] from-white dark:from-black/70">
           <div className="animate-trigger mx-auto max-w-6xl px-6 py-24 text-center md:px-12 xl:py-28">
             <h1 className="-mb-3 bg-gradient-to-b from-zinc-700 to-zinc-900 bg-clip-text pb-3 text-4xl font-extrabold text-transparent selection:bg-black/70 selection:text-white dark:from-white dark:from-40% dark:to-gray-400 dark:selection:bg-white/70 dark:selection:text-black md:text-5xl xl:text-6xl">
               Sync/Async
