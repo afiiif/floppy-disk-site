ded33cae639b335e43d2bf924c90000e2217ff78
M. Afifudin <afif.x.udin@gmail.com>
2026-04-06 17:57:37 +0700

    feat(example): add store example

--- examples/store/app/root.tsx
+++ examples/store/app/root.tsx
@@ -12,18 +12,7 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
+export const links: Route.LinksFunction = () => [];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
